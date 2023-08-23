0d29424306b9e33314892a6e3dd18bf2acc4341a
RamunnoAJ <ramunnoagustin@gmail.com>
2023-08-22 21:16:58 -0300

    update: files to use ESModules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const expresshbs = require('express-handlebars')
+import * as url from 'url'
+import fs from 'fs'
+import express from 'express'
+import expresshbs from 'express-handlebars'
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 const PORT = 8080
 
 const app = express()
@@ -15,6 +18,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
+  const teams = JSON.parse(fs.readFileSync('./data/equipos.json', 'utf8'))
+  console.log(teams[0])
   console.log('GET /')
   res.render('index', {})
 })
